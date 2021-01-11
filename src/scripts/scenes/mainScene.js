f107f6746a8211d8de1376e3d1b8293c994d41ec
Michal Marczynski <m.marczynski@mmit.tech>
2021-01-11 13:35:29 +0100

    Example

--- src/scripts/scenes/mainScene.js
+++ src/scripts/scenes/mainScene.js
@@ -1,50 +1,18 @@
-import PhaserLogo from '../objects/phaserLogo'
-import FpsText from '../objects/fpsText'
+// import PhaserLogo from '../objects/phaserLogo'
 
 export default class MainScene extends Phaser.Scene {
-  fpsText
-
   constructor() {
     super({ key: 'MainScene' })
   }
 
   create() {
-    /**
-     * Delete all the code below to start a fresh scene
-     */
-    new PhaserLogo(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
+    const map = this.make.tilemap({ key: 'map' });
+    const layer = map.createLayer('0', 'tileset', 0, 0);
+    const layer2 = map.createLayer('1', 'tileset', 0, 0);
 
-    // async/await example
-    const pause = ms => {
-      return new Promise(resolve => {
-        window.setTimeout(() => {
-          resolve()
-        }, ms)
-      })
-    }
-    const asyncFunction = async () => {
-      console.log('Before Pause')
-      await pause(4000) // 4 seconds pause
-      console.log('After Pause')
-    }
-    asyncFunction()
-
-    // Spread operator test
-    const numbers = [0, 1, 2, 3]
-    const moreNumbers = [...numbers, 4, 5]
-    console.log(`All numbers: ` + moreNumbers)
-
-    // display the Phaser.VERSION
-    this.add
-      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
-        color: '#000000',
-        fontSize: 24
-      })
-      .setOrigin(1, 0)
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   }
 
   update() {
-    this.fpsText.update()
   }
 }
--- src/scripts/scenes/preloadScene.js
+++ src/scripts/scenes/preloadScene.js
@@ -4,11 +4,11 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('phaser-logo', 'assets/img/phaser-logo.png')
+    this.load.tilemapTiledJSON('map', 'assets/map/map.json');
   }
 
   create() {
-    this.scene.start('MainScene')
+    this.scene.start('MainScene');
 
     /**
      * This is how you would dynamically import the mainScene class (with code splitting),
